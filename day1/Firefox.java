f566086f1d411b94a51df77fdc8fec23716ab209
Abinashsundarraj <168280708+Abinashsundarraj@users.noreply.github.com>
2024-04-27 18:55:01 +0530

    Add files via upload
    day1

---
+++ day1/Firefox.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		//datatypes variablenmae = value;
+		int  birthyear = 2001;
+		long phonenumber = 9014830332L;
+		float height = 5.9f;
+		double bankbalance = 10000.666d;
+		String hallticketnumber = "AP87526483";
+		boolean isavaiable =true;
+		System.out.println(birthyear);
+		System.out.println(phonenumber);
+		System.out.println(height);
+		System.out.println(isavaiable);
+
+	}
+
+}
